0eb0246c6404c2b4ae14b5dbe8e0f7783a5c5db7
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-11-18 10:25:08 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,25 @@
+public class RandomDataGenerator {
+    }
+}
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        }
+        }
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+
+    public static void main(String[] args) {        }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        for (int item : data) {
+    public static void main(String[] args) {
+    }
+        }
+}
+
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
